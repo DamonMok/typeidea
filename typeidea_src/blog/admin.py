1ec163d70a9deafc2eb36a60ac94dab8e2a2b2da
Damon <damonmok1216@gmail.com>
2019-11-30 16:00:50 +0800

    新增自定义过滤器、自定义列表页数据

--- typeidea_src/blog/admin.py
+++ typeidea_src/blog/admin.py
@@ -6,10 +6,28 @@ from .models import Post, Category, Tag
 
 # Register your models here.
 
+class CategoryOwnerFilter(admin.SimpleListFilter):
+	""" 自定义过滤器只展示当前用户分类 """
+
+	title = '分类过滤器'
+	parameter_name = 'owner_category'
+
+	def lookups(self, request, model_admin):
+		return Category.objects.filter(owner=request.user).values_list('id', 'name')
+
+	def queryset(self, request, queryset):
+		category_id = self.value()
+		if category_id:
+			return queryset.filter(category_id=self.value())
+		return queryset
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-	list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count', 'owner')
+	list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count')
+
 	fields = ('name', 'status', 'is_nav')
+
+	list_filter = [CategoryOwnerFilter]
 
 	def post_count(self, obj):
 		return obj.post_set.count()
@@ -34,12 +52,12 @@ class TagAdmin(admin.ModelAdmin):
 class PostAdmin(admin.ModelAdmin):
 	list_display = [
 		'title', 'category', 'status',
-		'created_time', 'operator'
+		'created_time', 'operator', 'owner'
 	]
 
 	list_display_links = []
 
-	list_filter = ['category', ]
+	list_filter = [CategoryOwnerFilter]
 	search_fields = ['title', 'category__name']
 
 	actions_on_top = True
@@ -62,6 +80,11 @@ class PostAdmin(admin.ModelAdmin):
 			reverse('admin:blog_post_change', args=(obj.id,))
 		)
 	operator.short_description='操作' # 表头展示名字
+
+	def get_queryset(self, request):
+		""" 根据权限展示文章 """
+		qs = super(PostAdmin, self).get_queryset(request)
+		return qs.filter(owner=request.user)
 
 	def save_model(self, request, obj, form, change):
 		obj.owner = request.user
