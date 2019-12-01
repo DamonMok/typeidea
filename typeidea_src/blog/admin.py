87a8fe162e2f7f87edf5ba6375bd1e8c6f9e3e20
DamonMok <damonmok1216@gmail.com>
2019-12-01 10:18:19 +0800

    自定义form

--- typeidea_src/blog/admin.py
+++ typeidea_src/blog/admin.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.utils.html import format_html
 
 from .models import Post, Category, Tag
+from .adminforms import PostAdminForm
 
 # Register your models here.
 
@@ -50,6 +51,10 @@ class TagAdmin(admin.ModelAdmin):
  
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+
+	# 自定义Form(desc摘要字段)
+	form = PostAdminForm
+
 	list_display = [
 		'title', 'category', 'status',
 		'created_time', 'operator', 'owner'
