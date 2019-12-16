3a337b4952c7897bae6524586b190a1f76829253
DamonMok <damonmok1216@gmail.com>
2019-12-16 16:54:39 +0800

    格式化代码

--- typeidea_src/blog/adminx.py
+++ typeidea_src/blog/adminx.py
@@ -1,160 +1,155 @@
-from django.contrib.admin.models import LogEntry
-
-from django.contrib import admin
-from django.urls import reverse
-from django.utils.html import format_html
-
-from .models import Post, Category, Tag
-from .adminforms import PostAdminForm
-from typeidea_src.base_admin import BaseOwnerAdmin
-from typeidea_src.custom_site import custom_site
-
 import xadmin
 from xadmin.filters import manager
 from xadmin.filters import RelatedFieldListFilter
 from xadmin.layout import Row, Fieldset, Container
 
+from django.urls import reverse
+from django.utils.html import format_html
+
+from typeidea_src.base_admin import BaseOwnerAdmin
+from .models import Post, Category, Tag
+from .adminforms import PostAdminForm
+
+
 # Register your models here.
 
 class CategoryOwnerFilter(RelatedFieldListFilter):
-	""" 自定义过滤器只展示当前用户分类 """
+    """ 自定义过滤器只展示当前用户分类 """
 
-	@classmethod
-	def test(cls, field, request, params, model, admin_view, field_path):
-		return field.name == 'category'
+    @classmethod
+    def test(cls, field, request, params, model, admin_view, field_path):
+        return field.name == 'category'
 
-	def __init__(self, field, request, params, model, model_admin, field_path):
-		super().__init__(field, request, params, model,model_admin, field_path)
-		# 重新获取 lookup_choice, 根据 owner 过滤
-		self.lookup_choices = Category.objects.filter(owner=request.user).values_list('id', 'name')
+    def __init__(self, field, request, params, model, model_admin, field_path):
+        super().__init__(field, request, params, model, model_admin, field_path)
+        # 重新获取 lookup_choice, 根据 owner 过滤
+        self.lookup_choices = Category.objects.filter(owner=request.user).values_list('id', 'name')
 
 manager.register(CategoryOwnerFilter, take_priority=True)
 
 
 class PostInline():
-	# 在 分类详细页 内置关联数据() 文章编辑
-	form_layout = (
-		Container(
-			Row("title", "desc",)
-		)
-	)
-	extra = 1 # 控制额外多几个
-	model = Post
+    # 在 分类详细页 内置关联数据() 文章编辑
+    form_layout = (
+        Container(
+            Row("title", "desc",)
+        )
+    )
+    extra = 1 # 控制额外多几个
+    model = Post
 
 
 @xadmin.sites.register(Category)
 class CategoryAdmin(BaseOwnerAdmin):
 
-	inlines = [PostInline, ] # 在 分类详细页 内置 文章编辑
+    inlines = [PostInline, ] # 在 分类详细页 内置 文章编辑
 
-	list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count')
+    list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count')
 
-	fields = ('name', 'status', 'is_nav')
+    fields = ('name', 'status', 'is_nav')
 
 
-	def post_count(self, obj):
-		return obj.post_set.count()
-	post_count.short_description = '文章数量'
+    def post_count(self, obj):
+        return obj.post_set.count()
+    post_count.short_description = '文章数量'
 
 
 
 @xadmin.sites.register(Tag)
 class TagAdmin(BaseOwnerAdmin):
-	list_display = ('name', 'status', 'created_time')
-	fields = ('name', 'status')
+    list_display = ('name', 'status', 'created_time')
+    fields = ('name', 'status')
 
 
 
- 
+
 @xadmin.sites.register(Post)
 class PostAdmin(BaseOwnerAdmin):
 
-	# 自定义Form(desc摘要字段)
-	form = PostAdminForm
+    # 自定义Form(desc摘要字段)
+    form = PostAdminForm
 
-	list_display = [
-		'title', 'category', 'status',
-		'created_time', 'operator', 'owner'
-	]
+    list_display = [
+        'title', 'category', 'status',
+        'created_time', 'operator', 'owner'
+    ]
 
-	list_display_links = []
+    list_display_links = []
 
-	list_filter = ['category']
-	search_fields = ['title', 'category__name']
+    list_filter = ['category']
+    search_fields = ['title', 'category__name']
 
-	actions_on_top = True
-	actions_on_bottom = True
+    actions_on_top = True
+    actions_on_bottom = True
 
- 	# 编辑页面
-	save_on_top = True
+     # 编辑页面
+    save_on_top = True
 
-	# 限定要展示的字段、字段的顺序
-	# fields = (
-	# 	('category', 'title'),
-	# 	'desc',
-	# 	'status',
-	# 	'content',
-	# 	'tag',
-	# )
-	
-	# 控制页面的布局
-	
-	form_layout = (
-		Fieldset(
-			'基础信息',
-			Row("title", "category"),
-			'status',
-			'tag',
-		),
-		Fieldset(
-			'内容信息',
-			'desc',
-			'content',
-		)
-	)
+    # 限定要展示的字段、字段的顺序
+    # fields = (
+    #     ('category', 'title'),
+    #     'desc',
+    #     'status',
+    #     'content',
+    #     'tag',
+    # )
 
-	filter_horizontal = ('tag',) # 多对多控制横竖布局
+    # 控制页面的布局
+    form_layout = (
+        Fieldset(
+            '基础信息',
+            Row("title", "category"),
+            'status',
+            'tag',
+        ),
+        Fieldset(
+            '内容信息',
+            'desc',
+            'content',
+        )
+    )
 
-	def operator(self, obj):
-		return format_html(
-			'<a href="{}">编辑</a>',
-			reverse('xadmin:blog_post_change', args=(obj.id,))
-		)
-	operator.short_description='操作' # 表头展示名字
+    filter_horizontal = ('tag',) # 多对多控制横竖布局
 
-	"""
-	被抽象到BaseownerAdmin
-	def get_queryset(self, request):
-		# 根据权限展示文章
-		qs = super(PostAdmin, self).get_queryset(request)
-		return qs.filter(owner=request.user)
+    def operator(self, obj):
+        return format_html(
+            '<a href="{}">编辑</a>',
+            reverse('xadmin:blog_post_change', args=(obj.id,))
+        )
+    operator.short_description = '操作' # 表头展示名字
 
-	def save_model(self, request, obj, form, change):
-		# owner字段 根据当前用户自动填写
-		obj.owner = request.user
-		return super(PostAdmin, self).save_model(request, obj, form, change)
-	"""
+    """
+    被抽象到BaseownerAdmin
+    def get_queryset(self, request):
+        # 根据权限展示文章
+        qs = super(PostAdmin, self).get_queryset(request)
+        return qs.filter(owner=request.user)
 
-	# (Django自带)添加自定义的JavaScript和CSS
-	# class Media:
-	# 	css = {
-	# 		'all': ("https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css", ),
-	# 	}
+    def save_model(self, request, obj, form, change):
+        # owner字段 根据当前用户自动填写
+        obj.owner = request.user
+        return super(PostAdmin, self).save_model(request, obj, form, change)
+    """
 
-	# 	js = ('https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap/bundle.js', )
-	
-	# @property
-	# def media(self):
-	# 	media = super().media
-	# 	media.add_js(['https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap/bundle.js'])
-	# 	media.add_css({
-	# 		'all': ("https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css", ),
-	# 	})
-	# 	return media
+    # (Django自带)添加自定义的JavaScript和CSS
+    # class Media:
+    #     css = {
+    #         'all': ("https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css", ),
+    #     }
+
+    #     js = ('https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap/bundle.js', )
+    #     # @property
+    # def media(self):
+    #     media = super().media
+    #     media.add_js(['https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap/bundle.js'])
+    #     media.add_css({
+    #         'all': ("https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css", ),
+    #     })
+    # return media
 
 
 # Django的Log配置
 # @xadmin.sites.register(LogEntry)
 # class LogEntryAdmin(object):
-# 	list_display = ['object_repr', 'object_id', 'action_flag', 'user',
-# 		'change_message']
+#     list_display = ['object_repr', 'object_id', 'action_flag', 'user',
+#         'change_message']
--- typeidea_src/comment/adminx.py
+++ typeidea_src/comment/adminx.py
@@ -1,12 +1,9 @@
-from django.contrib import admin
+import xadmin
 
 from .models import Comment
 
-from typeidea_src.custom_site import custom_site
-
-import xadmin
 
 # Register your models here.
 @xadmin.sites.register(Comment)
-class CommentAdmin(object): 
-	list_display = ('target', 'nickname', 'content', 'website', 'created_time')
+class CommentAdmin(object):
+    list_display = ('target', 'nickname', 'content', 'website', 'created_time')
