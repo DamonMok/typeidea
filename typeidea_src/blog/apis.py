aa5d1706e75823e59b0ad0d0f5321e1067fea51b
DamonMok <damonmok1216@gmail.com>
2019-12-18 15:25:32 +0800

    区分list和detail返回接口内容

--- typeidea_src/blog/apis.py
+++ typeidea_src/blog/apis.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser
 
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, PostDetailSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -11,3 +11,8 @@ class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=Post.STATUS_NORMAL)
     # permission_classes = [IsAdminUser] # 写入时的权限校验
+
+    """ 重写详情接口 """
+    def retrieve(self, request, *args, **kwargs):
+        self.serializer_class = PostDetailSerializer
+        return super().retrieve(request, *args, **kwargs)
