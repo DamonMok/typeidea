bf7b40954c389e17c1980d1ffc5c811e25697b43
DamonMok <damonmok1216@gmail.com>
2019-12-21 13:36:10 +0800

    配置MySQL数据库

--- typeidea_src/typeidea_src/settings/develop.py
+++ typeidea_src/typeidea_src/settings/develop.py
@@ -5,9 +5,24 @@ DEBUG = True
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
+ """
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'typeidea_db',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        # 'CONN_MAX_AGE': 5 * 60,
+        # 'OPTIONS': {'charset': 'utf8mb4'}
+    },
 }
